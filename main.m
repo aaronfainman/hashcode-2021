0d6d3b6ede70eb2a7f0809dfbaf73ad3f02be878
Basheq Tarifi <btarifi08@gmail.com>
2021-02-25 23:24:51 +0200

    main

--- main.m
+++ main.m
@@ -1,4 +1,9 @@
-fileName = "a.txt";
+fileName = "d.txt";
 
 [D, numIntersections, streets, carPaths, points] = hashcodeRead(fileName);
 
+outData = findSingleIntersections(streets, numIntersections);
+
+fileOut = "d_out.txt";
+
+outputData(fileOut, outData);
